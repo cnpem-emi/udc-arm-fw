8063053b511cd851475ec66bf398723a62e87876
Lucas <lucas.carnevalli@cnpem.br>
2026-06-01 10:45:42 -0300

    Resonant WEG version 1.0
    Update version and folder name.

--- app/board_drivers/version.c
+++ app/board_drivers/version.c
@@ -23,4 +23,4 @@
 
 volatile firmwares_version_t firmwares_version;
 
-const char * udc_arm_version = "0.48.00swls02/26";
+const char * udc_arm_version = "0.49.00weg05/25";
--- main.c
+++ main.c
@@ -77,7 +77,7 @@
 #include "communication_drivers/ps_modules/fap_4p/fap_4p.h"
 #include "communication_drivers/ps_modules/fbp/fbp.h"
 #include "communication_drivers/ps_modules/fbp_dclink/fbp_dclink.h"
-#include "communication_drivers/ps_modules/resonant_swls/resonant_swls.h"
+#include "communication_drivers/ps_modules/resonant_weg/resonant_weg.h"
 
 extern unsigned long RamfuncsLoadStart;
 extern unsigned long RamfuncsRunStart;
@@ -216,9 +216,9 @@ int main(void) {
             break;
         }
 
-        case RESONANT_SWLS:
+        case RESONANT_WEG:
         {
-            resonant_swls_system_config();
+            resonant_weg_system_config();
             break;
         }
 
